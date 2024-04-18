551c5b50fbd94b12ba541a359099c35270dcc19b
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2024-04-18 15:08:57 -0400

    Add files via upload

--- PlaybackController.java
+++ PlaybackController.java
@@ -1,4 +1,25 @@
 
 public class PlaybackController {
+	
+	 public int audioLength; 
+	 public int playbackStartingTime; 
+	 public boolean pausePlayBool;
+	 
+	 private int userChoice;
+	 
+	 public boolean isPaused(int userChoice) {
+		 if(userChoice == 0) {
+			 pausePlayBool = true;
+		 }
+		 else {
+			 pausePlayBool = false;
+		 }
+		 
+		 return pausePlayBool;
+	 }
 
+	 public void seek() {
+		 
+	 }
 }
+
