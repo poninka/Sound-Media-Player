5af3acc630e8ff03c22789c26e0b92400db9d830
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2024-05-05 21:13:45 -0400

    Update PlaybackController.java

--- PlaybackController.java
+++ PlaybackController.java
@@ -1,29 +1,66 @@
-//Test - Daguilar0123 made changes to this file on my mac on eclipse
 public class PlaybackController {
+
+    private SongDataFetcher songDataFetcher;
+    private boolean isPlaying;
+    private int songSeekSlider;
+    
+    public PlaybackController(SongDataFetcher songDataFetcher) 
+    {
+        this.songDataFetcher = songDataFetcher;
+        this.isPlaying = false;
+        this.songSeekSlider = 0;
+    }
 	
-	 public int audioLength; 
-	 public int playbackStartingTime; 
-	 public boolean pausePlayBool;
-	 
-	 private int userChoice;
-	 
-	 public boolean isPaused(int userChoice) {
-		 if(userChoice == 0) {
-			 pausePlayBool = true;
-		 }
-		 else {
-			 pausePlayBool = false;
-		 }
-		 
-		 return pausePlayBool;
-	 }
-
-	 public void seek() {
-		 
-		  currentPos = playbackStartingTime;
-		 
-		 while(currentPos < audioLength) {
-			 currentPos++;
-	 }
+    public void togglePlayPause() {
+        if (isPlaying) {
+            pausePlayback();
+        } else {
+            startPlayback();
+        }
+    }
+    
+    // Code to start playback using the Spotify API
+    public void startPlayback() {
+       
+        // Update the isPlaying flag
+        isPlaying = true;
+    }
+    
+    // Code to pause playback using the Spotify API
+    public void pausePlayback() {
+       
+        // Update the isPlaying flag
+        isPlaying = false;
+    }
+    
+    // Code to skip to the next track using the Spotify API
+    public void forwardTrack() {
+   
+        // Fetch and display the new track information
+        songDataFetcher.fetchAndDisplaySongInfo("new_track_id");
+        resetSeekPosition();
+    }
+    
+    // Code to go back to the previous track using the Spotify API
+    public void backwardTrack() {
+        
+        // Fetch and display the new track information
+        songDataFetcher.fetchAndDisplaySongInfo("previous_track_id");
+        resetSeekPosition();
+    }
+    
+    // Code to seek to the specified position in the current track using the Spotify API
+    private void seekToPosition(int position) {
+        
+        // Update the songSeekSlider
+        songSeekSlider = position;
+    }
+    
+    // Reset the seek position to the beginning of the track
+    private void resetSeekPosition() {
+    	
+    	//resets the songSeekSlider
+        songSeekSlider = 0;
+    
+    }
 }
-
