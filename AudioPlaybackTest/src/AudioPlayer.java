a5f4b7da27d7ddb1a560297702e54babd4f99bc5
ItsDan <dbarahona0317@gmail.com>
2024-05-07 15:12:29 -0400

    Update AudioPlayer.java

--- AudioPlaybackTest/src/AudioPlayer.java
+++ AudioPlaybackTest/src/AudioPlayer.java
@@ -1,39 +1,128 @@
 import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JOptionPane;
-// Establish Audio Player Class which Has a temp main built to show how it works
-// - Dan A B
-public class AudioPlayer{
-	public static void main(String args[]) {
-		// Uses string variable to write the filepath but could be replaced with other input
-		// These Java audio systems only work with .wav files for no
-		String filepath = "MusicTestFile.wav";
-		// Calls the created PlayMusic Class to insert the given file path into the controller
-		PlayMusic(filepath);
-		// Temp way to stop audio as Java Garbage collection stops automatically without something else running
-		JOptionPane.showMessageDialog(null,"Press OK to Stop music");
-	}
+import javax.sound.sampled.Clip;
 
-	// PlayMusic Class: 
-	// Functions by Testing if file given exists, if it does it will try to play the audio by inserting
-	// the file path into the java AudioSystem library with an audioInput Object taking the file input from the 
-	// filepath string > musicPath (File Object) > AudioSystem Clip to run the function .open(AudioInput), then .start
-	// if try fails it returns File Error
-	public static void PlayMusic(String location) {
+public class AudioPlayer {
+	
+	// Audio File Filter marks String to WAV or MP3 depending on 
+	// corresponding file type.
+    String filePath;
+    private String fileType;
+	private int audioLength;
+	
+	public void playWav(String inputFilePath) {
+		this.filePath = inputFilePath;
 	try {
-		File musicPath = new File(location);
+		File musicPath = new File(inputFilePath);
 		
 		if (musicPath.exists()) {
 			AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
 			Clip musicClip = AudioSystem.getClip();
 			musicClip.open(audioInput);
 			musicClip.start();
-		}else {System.out.println("File Error, nothing played");}
+			JOptionPane.showMessageDialog(null,"Press OK to Stop music");
+		}else{JOptionPane.showMessageDialog(null,"Error File Not Found");}
 		
 	}catch(Exception e) {
 		System.out.println(e);
 		}
+	}
+
+
+public void getLength(String inputFilePath, String fileType) {
+	this.filePath = inputFilePath;
+	this.fileType = fileType;
+	
+		if (fileType == "WAV") {
+			File file = new File(inputFilePath);
+			AudioInputStream audioInputStream = null;
+			try {
+				audioInputStream = AudioSystem.getAudioInputStream(file);
+			} catch (UnsupportedAudioFileException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			AudioFormat format = audioInputStream.getFormat();
+			long frames = audioInputStream.getFrameLength();
+			double SecondsRaw = (frames+0.0) / format.getFrameRate();
+			double Seconds = Math.round(SecondsRaw * 100.00) / 100.00;
+			this.audioLength = (int)Seconds;
+
+			System.out.println(audioLength);
+			System.out.println(fileType);
+			}
+	
+	if (fileType == "MP3") {
+		System.out.println("MP3 LENGTH NOT FINISHED");
+		System.out.println(fileType);
+	}
+}
+
+public void playSong(String inputFilePath, String fileType) {
+	this.filePath = inputFilePath;
+	this.fileType = fileType;
+	
+	if (fileType == "WAV") {
+		playWav(filePath);
+	}
+	if (fileType == "MP3") {
+		System.out.println("PLAYBACK NOT FINISHED FOR MP3");
+		System.out.println(fileType);
+	}
+}
+
+	
+    public String getFileType(String inputFilePath) {
+        this.filePath = inputFilePath;
+        File audioFile = new File(filePath);
+        
+        FileFilter wavFileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.getName().toLowerCase().endsWith(".wav");
+            }
+        };
+        
+        FileFilter mp3FileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.getName().toLowerCase().endsWith(".mp3");
+            }
+        };
+
+        if (wavFileFilter.accept(audioFile)) {
+            return "WAV";
+        } else if (mp3FileFilter.accept(audioFile)) {
+            return "MP3";
+        } else {
+            return "Unsupported File Type";
+        }
+    } 
+    
+  // Main that controls the INPUT of files and gets the type and can send to the player.
+	public static void main(String args[]) {	
+		
+		String inputFilePath = "AudioFiles/rock.wav";
+		
+		AudioPlayer AudioFile = new AudioPlayer();
+		String fileType = AudioFile.getFileType(inputFilePath);
+		
+
+		AudioFile.getLength(inputFilePath,fileType.toString());
+		AudioFile.playSong(inputFilePath, fileType.toString());
+		
+		System.out.println();
+
 	}
 }
