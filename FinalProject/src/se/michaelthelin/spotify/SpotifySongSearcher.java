137f9c105f4e863901b09c7ec4128e35448e42ba
avinjoby <144379366+avinjoby@users.noreply.github.com>
2024-05-07 21:19:04 -0400

    Add files via upload

---
+++ FinalProject/src/se/michaelthelin/spotify/SpotifySongSearcher.java
@@ -0,0 +1,41 @@
+package se.michaelthelin.spotify;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import okhttp3.*;
+
+public class SpotifySongSearcher {
+    private final OkHttpClient httpClient = new OkHttpClient();
+    private final String baseApiUrl = "https://api.spotify.com/v1";
+
+    public String searchSong(String accessToken, String songName) {
+        try {
+        	 HttpUrl url = HttpUrl.parse(baseApiUrl + "/search").newBuilder()
+                     .addQueryParameter("q", songName)
+                     .addQueryParameter("type", "track")
+                     .build();
+
+             Request request = new Request.Builder()
+                     .url(url)
+                     .header("Authorization", "Bearer " + accessToken)
+                     .build();
+
+             Response response = httpClient.newCall(request).execute();
+             String jsonResponse = response.body().string();
+
+             // Parse the JSON response to get the track ID
+             JsonElement root = JsonParser.parseString(jsonResponse);
+             String trackId = root.getAsJsonObject()
+                     .get("tracks").getAsJsonObject()
+                     .get("items").getAsJsonArray()
+                     .get(0).getAsJsonObject()
+                     .get("id").getAsString();
+
+             return trackId;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
