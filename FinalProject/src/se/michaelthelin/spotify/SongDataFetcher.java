d8b41fe8088b9610e4211171af5c2ddea307b29e
Daguilar0123 <144361178+Daguilar0123@users.noreply.github.com>
2024-05-14 11:30:20 -0400

    Update SongDataFetcher.java
    partial comments. in progress.

--- FinalProject/src/se/michaelthelin/spotify/SongDataFetcher.java
+++ FinalProject/src/se/michaelthelin/spotify/SongDataFetcher.java
@@ -9,29 +9,39 @@ import java.util.Scanner;
 
 public class SongDataFetcher {
 
+    // Static fields for Spotify API client and song searcher
     private static SpotifyApiClient spotifyApiClient;
     private static SpotifySongSearcher spotifySongSearcher;
 
+    // Contstructor to initialize the Spotify API client and song searcher
     public SongDataFetcher() {
         spotifyApiClient = new SpotifyApiClient();
         spotifySongSearcher = new SpotifySongSearcher();
     }
 
+    // Method to fetch and display song information
     public void fetchAndDisplaySongInfo(String songName) {
+        // Get Access token from Spotify API Client
         String accessToken = spotifyApiClient.getAccessToken();
+
+        // Search for the song using the access token and song name
         String trackId = spotifySongSearcher.searchSong(accessToken, songName);
 
         if (accessToken != null) {
+            // Get track data using the acces token and track ID
             String trackData = spotifyApiClient.getTrackData(accessToken, trackId);
             if (trackData != null) {
                 // Parse trackData to extract song information
                 Gson gson = new Gson();
                 JsonObject trackJson = gson.fromJson(trackData, JsonObject.class);
 
+                // Extract song details from the JSON object
                 String title = trackJson.get("name").getAsString();
                 String artist = trackJson.getAsJsonArray("artists").get(0).getAsJsonObject().get("name").getAsString();
                 String coverArtPath = trackJson.getAsJsonObject("album").getAsJsonArray("images").get(0).getAsJsonObject().get("url").getAsString();
                 int songLength = trackJson.get("duration_ms").getAsInt() / 1000; // converting milliseconds to seconds
+
+                // Display the song information
                 System.out.println(" Title: " + title + "\n Artist: " +  artist + "\n Image: " + coverArtPath + "\n Duration: " + songLength + "s");
             } else {
                 System.out.println("Failed to fetch track data from Spotify API.");
